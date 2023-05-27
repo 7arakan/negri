7f600cdbb9df978f33a02cd9a98fd108b1e71756
zefirk <zefirk20@gmail.com>
2023-05-27 11:08:01 +0300

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  let [blockClass, setClassBlock] = React.useState('block')
+  let [blockText, setBlockText] = React.useState('modomi')
+  let [ulClass, setClassUl] = React.useState('ul')
+  let [headerClass, setClassHeader] = React.useState('header')
+  let [headerHClass, setClassHeaderH] = React.useState('headerH')
+  let [headerText, setHeaderText] = React.useState('Заголовок')
+
+  const headerClick = () =>{
+      setClassHeader('header-clicked')
+      setClassHeaderH('headerH-clicked')
+      setHeaderText('Не заголовок')
+  }
+  const blockClick = () =>{
+      setClassBlock('block-clicked')
+      setBlockText('modomiku')
+  }
+  const ulClick = () =>{
+      setClassUl('ul-clicked')
+  }
+  return (
+      <div className="app">
+          <div className={headerClass} onClick={headerClick}><h1 className={headerHClass}>{headerText}</h1></div> 
+          <div className='container2'>
+
+              <ul className={ulClass} onClick={ulClick}>
+                  <li>text1</li>
+                  <li>text2</li>
+                  <li>text3</li>
+                  <li>text4</li>
+              </ul>
+              <div className={blockClass} onClick={blockClick}>{blockText}</div>
+          </div>
+      </div>
+  )
+}
+const container = document.querySelector('#root')
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
